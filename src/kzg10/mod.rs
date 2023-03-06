bb434b5d908d124bb74cf07dd0cfc14d4d4ba5d9
jdetchart <jdetchart@users.noreply.github.com>
2023-03-06 17:16:40 +0100

    Update kzg10/mod.rs
    Set to visible every function inside the KZG10 struct

--- src/kzg10/mod.rs
+++ src/kzg10/mod.rs
@@ -260,7 +260,7 @@ where
         Ok((witness_polynomial, random_witness_polynomial))
     }
 
-    pub(crate) fn open_with_witness_polynomial<'a>(
+    pub fn open_with_witness_polynomial<'a>(
         powers: &Powers<E>,
         point: P::Point,
         randomness: &Randomness<E::ScalarField, P>,
@@ -304,7 +304,7 @@ where
     }
 
     /// On input a polynomial `p` and a point `point`, outputs a proof for the same.
-    pub(crate) fn open<'a>(
+    pub fn open<'a>(
         powers: &Powers<E>,
         p: &P,
         point: P::Point,
@@ -410,7 +410,7 @@ where
         Ok(result)
     }
 
-    pub(crate) fn check_degree_is_too_large(degree: usize, num_powers: usize) -> Result<(), Error> {
+    pub fn check_degree_is_too_large(degree: usize, num_powers: usize) -> Result<(), Error> {
         let num_coefficients = degree + 1;
         if num_coefficients > num_powers {
             Err(Error::TooManyCoefficients {
@@ -422,7 +422,7 @@ where
         }
     }
 
-    pub(crate) fn check_hiding_bound(
+    pub fn check_hiding_bound(
         hiding_poly_degree: usize,
         num_powers: usize,
     ) -> Result<(), Error> {
@@ -441,7 +441,7 @@ where
         }
     }
 
-    pub(crate) fn check_degrees_and_bounds<'a>(
+    pub fn check_degrees_and_bounds<'a>(
         supported_degree: usize,
         max_degree: usize,
         enforced_degree_bounds: Option<&[usize]>,
